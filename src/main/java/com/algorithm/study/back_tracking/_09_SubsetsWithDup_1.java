5331c5fa8cae25113cc050a1756ce23080891041
fuguangwei <fuguangwei@xg.com>
2022-05-12 09:46:26 +0800

    task: 0514 (27/62) back-tracking: 78. 子集 cavs-coder Today 0:02

---
+++ src/main/java/com/algorithm/study/back_tracking/_09_SubsetsWithDup_1.java
@@ -0,0 +1,39 @@
+package com.algorithm.study.back_tracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 90. 子集 II
+ * 所有树枝路径（根到叶树枝路径可以重复） + 每层路径不重复计算（同一树层不可以重复）
+ */
+public class _09_SubsetsWithDup_1 {
+
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backtracking(nums, 0);
+        return result;
+    }
+
+    private void backtracking(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(path));
+        if (startIndex == nums.length) {
+            return;
+        }
+
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            path.add(nums[i]);
+            backtracking(nums, i + 1);
+            path.removeLast();
+        }
+    }
+}
