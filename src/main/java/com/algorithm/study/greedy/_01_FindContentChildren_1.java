99503fc250a802704acf6ebd5a01eed1c58c7ecc
cavs-coder <185286127@qq.com>
2022-05-16 00:44:31 +0800

    task: 0517 (34/62) greedy: 455. 分发饼干

---
+++ src/main/java/com/algorithm/study/greedy/_01_FindContentChildren_1.java
@@ -0,0 +1,35 @@
+package com.algorithm.study.greedy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 455. 分发饼干
+ */
+public class _01_FindContentChildren_1 {
+
+    public int findContentChildren(int[] g, int[] s) {
+        if (s.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        List<Integer> list = Arrays.stream(s).boxed().sorted(Comparator.comparing(Integer::intValue).reversed()).collect(Collectors.toList());
+        Arrays.sort(g);
+        for (int i = g.length - 1; i >= 0; i--) {
+            Iterator<Integer> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                if (g[i] <= iterator.next()) {
+                    count++;
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
